19c045526f3641b34cffc44c4965349f727a905c
luoyi99 <luoyi@uw.edu>
2024-02-03 16:15:54 -0800

    edited worldclock

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
-"""
-Change this to a world clock app
-"""
-
-import time
 import streamlit as st
+import time
 import datetime
 import zoneinfo
-
 
 st.title("World Clock")
 
 zone_list = zoneinfo.available_timezones()
-
-selected_zone = st.multiselect(
+selected_zones = st.multiselect(
     'Which time zones do you want to see?',
-     zone_list,default = ['America/Los_Angeles'])
+    zone_list,
+    default = ['America/Los_Angeles'], 
+    max_selections=4)
+
+tab1, tab2 = st.tabs(["standard format", "UNIX format"])
+time_placeholders_1 = [tab1.empty() for _ in selected_zones]
+time_placeholders_2 = [tab2.empty() for _ in selected_zones]
 
 
-for zone in selected_zone:
-    st.markdown("zone: " + zone)
-    time = now.astimezone(zoneinfo.ZoneInfo(zone))
-    st.markdown(time.strftime("%Y-%m-%d %H:%M:%S"))
+cnt = 0
+    
 
-
-# now.astimezone(zoneinfo.ZoneInfo(zone))
-
-# placeholder = st.empty()
-
-# cnt = 0
-# while True:
-#     with placeholder.container():
-#         placeholder.metric("Seconds since you arrived this page", cnt)
-#         cnt += 1
-        
-#     time.sleep(1)
+while True:
+    for i, zone in enumerate(selected_zones):
+        current_time = datetime.datetime.now(tz=zoneinfo.ZoneInfo(zone))
+        time_placeholders_1[i].metric(label=f"{zone}: ",value=f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}")
+        time_placeholders_2[i].metric(label=f"{zone}: ",value=f"{current_time.timestamp()}")
+    # Wait for 1 second before updating again
+    time.sleep(1)
+    
