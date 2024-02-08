5d692d24c2301f6c2e34e2bc3d91276141172691
luoyi99 <luoyi@uw.edu>
2024-02-07 22:50:22 -0800

    added currency converter feature

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # TECHIN 510 Lab 4 
+A world clock app that displays time in different locations around the world
 
-## Regular expression, datetime, and real-time data
+## How to run
+**Method 1:**
+Open [luoyi-techin510-lab4.azurewebsites.net](luoyi-techin510-lab4.azurewebsites.net) in brower
 
-### Overview
+**Method 2:**
+Open the terminal and run the following commands:
+```
+pip install -r requirements.txt
+streamlit run app.py
+```
 
-- datetime.ipynb: This notebook contains the code for the datetime exercise.
-- regex.ipynb: This notebook contains the code for the regex exercise.
-- app.py: Example world clock app.
+### What's Included
+
+- `datetime.ipynb`: Contains the code for the datetime exercise.
+- `regex.ipynb`: Contains the code for the regex exercise.
+- `app.py`: The world clock app.
+- `hello.py`: A hello world file to test github action
+- `requirements.txt`: Required packages to run the application
 
 ### Lessions Learned
+- How to use zoneinfo to convert time in different time zone
+- How to use datetime to parse time string and print datetime object in different format
+- How to create tabs in streamlit app
+- How to use github action for script automation
 
 ### Questions
+- I am still a little confused about the purpose of using PostgreSQL, what is its benefit? How is it different from the sqlite?
+- If I want to show the live exchange rate between USD and the currency in the selected time zone, what would be a good way to achieve this goal?
--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import time
 import datetime
 import zoneinfo
+import freecurrencyapi as fca
+
+# Connect to the Free Currency API
+client = fca.Client("fca_live_lvjvJUaMcXEk1NXMpxsA3UmjMbOl3llsqdCUXK12")
 
 st.title("World Clock")
 
@@ -16,10 +20,42 @@ tab1, tab2 = st.tabs(["standard format", "UNIX format"])
 time_placeholders_1 = [tab1.empty() for _ in selected_zones]
 time_placeholders_2 = [tab2.empty() for _ in selected_zones]
 
+st.divider()
+
+# Currency Converter
+st.header("Currency Converter")
+currency_list = ["EUR", "USD", "JPY", "BGN", "CZK", "DKK", "GBP", "HUF", "PLN", "RON", "SEK", "CHF", "ISK", "NOK", "HRK", "RUB", "TRY", "AUD", "BRL", "CAD", "CNY", "HKD", "IDR", "ILS", "INR", "KRW", "MXN", "MYR", "NZD", "PHP", "SGD", "THB", "ZAR"]
+currency_list = client.currencies(currencies=currency_list)
+
+currency_options = [f"{code} - {info['name']}" for code, info in currency_list['data'].items()]
+
+col1, col2, col3 = st.columns([0.45,0.1,0.45])
+
+with col1:
+   option1 = st.selectbox('Select base currency', list(currency_options),key='currency1', index=1)
+   st.write(f"1 {option1[:3]}")
+   
+
+with col3:
+   option2 = st.selectbox('Select the currency you want to convert to', list(currency_options),key='currency2', index=0)
+   exchange_rate = client.latest(base_currency=option1[:3], currencies=[option2[:3]])
+   exchange_rate = exchange_rate['data'][option2[:3]]
+   st.write(f"{exchange_rate} {option2[:3]}")
+
+
+
+col4, col5, col6 = st.columns([0.45,0.1,0.45])   
+with col4:
+    number = st.number_input('Enter a number')
+
+with col5:
+    st.markdown("## => ##")
+
+with col6:
+    st.write("Converted Value")
+    st.write(exchange_rate * number)
 
 cnt = 0
-    
-
 while True:
     for i, zone in enumerate(selected_zones):
         current_time = datetime.datetime.now(tz=zoneinfo.ZoneInfo(zone))
@@ -27,4 +63,4 @@ while True:
         time_placeholders_2[i].metric(label=f"{zone}: ",value=f"{current_time.timestamp()}")
     # Wait for 1 second before updating again
     time.sleep(1)
-    
+
